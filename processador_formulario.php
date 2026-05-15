4b33c939d75219300aa39d02b1d393b3b85840fd
Otavio-2010 <otaviosferraz@gmail.com>
2026-05-15 15:14:09 -0300

    Add files via upload

---
+++ processador_formulario.php
@@ -0,0 +1,40 @@
+<?php
+
+$mes = $_REQUEST['campo_mes'];
+$dia = $_REQUEST['campo_dia'];
+$ano = $_REQUEST['campo_ano'];
+
+function somatorio(int $n){
+    $soma = 0;
+    for($i = 1; $i <= $n; $i++){
+        $soma += $i;
+    }
+    return $soma;
+}
+
+function somaDigitos(int $num){
+
+    $soma = 0;
+    while($num > 0){
+        $digito = $num % 10;
+        $soma += $digito;
+        $num = (int)($num / 10);
+    }
+    return $soma;
+
+}
+
+
+function safadao($mes, $dia, $ano){
+
+$safadeza = 0;
+
+$safadeza = somatorio($mes) + ((somaDigitos($ano) * (50 - $dia))/10);
+
+$anjo = 0;
+$anjo = 100 - $safadeza;
+
+echo "Não anjo: $safadeza% <br>";
+echo "Anjo: $anjo%";
+
+}
